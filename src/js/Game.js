6735e3ef41b1c95b983c9581249ab6c85589dcb1
Mgt03 <94318733+Mgt03@users.noreply.github.com>
2022-03-03 09:28:41 +0100

    Création des class word et game, et ajout d'un fichier de constante

---
+++ src/js/Game.js
@@ -0,0 +1,19 @@
+class Game{
+    TryToSuccess;
+    DateGame = new Date();
+    WordToFind = new Word("toto");
+    Succed;
+    
+    FirstLetter(){
+        return this.WordToFind[0];
+    }
+
+    isWordFound(word){
+        WordToFind.Compare(word);
+        if(!WordToFind.tabPositions.includes(0)){
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ src/js/Word.js
@@ -0,0 +1,31 @@
+class Word{
+    word;
+    tabPositions = []; //0 if not good letter, 1 otherwise
+    goodLetters = []; //list of the good letter in wrong position
+    constructor(word) {
+        this.word = word;
+    }
+
+    GoodPositions(word){
+        for (var i=0;i<word.length;i++) {
+            if (word[i]===this.word[i]) {
+                this.tabPositions.push(1);
+            }
+            else{
+                GoodLetters(word[i]);
+                this.tabPositions.push(0);
+            }
+        }
+    }
+
+    GoodLetters(letter){
+        if(this.word.includes(letter)){
+            this.goodLetters.push(letter);
+        }
+    }
+
+    Compare(word){
+        GoodPositions(word);
+        console.log(this.goodLetters,this.tabPositions);
+    }
+}
