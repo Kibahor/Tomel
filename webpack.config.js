414f98fda02b335e8218bab88624841ff556ce63
Lelexxx <rruiz.alex@gmail.com>
2022-05-08 12:29:03 +0000

    Copy files with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
 const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+
+const distFolder = 'dist/';
+const cssDistFolder = 'css/';
+const jsDistFolder = 'js/';
+const imgDistFolder = 'img/';
 
 module.exports = {
     mode: 'production',
@@ -13,6 +19,18 @@ module.exports = {
     },
     output: {
         filename: 'App.min.js',
-        path: path.resolve(__dirname, 'dist/js'),
+        path: path.resolve(__dirname, distFolder),
     },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: 'node_modules/bootstrap/dist/css/bootstrap.min.css', to: cssDistFolder },
+                { from: 'node_modules/bootstrap/dist/css/bootstrap.min.css.map', to: cssDistFolder },
+                { from: 'src/css/style.css', to: cssDistFolder },
+                { from: 'src/img/tomel-favicon.png', to: imgDistFolder },
+                { from: 'src/img/tomel-logo.png', to: imgDistFolder },
+                { from: 'node_modules/vue/dist/vue.global.prod.js', to: jsDistFolder }
+            ],
+        }),
+    ],
 };
