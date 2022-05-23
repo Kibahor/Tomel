b1ab957af603c13c6ed067e42cd1dbe0e8fea7ef
nozolger <normann.zolger@etu.uca.fr>
2022-05-23 21:10:23 +0200

    verification alpha

--- view/components/Case.js
+++ view/components/Case.js
@@ -39,12 +39,20 @@ export default {
                 throw 'Veuillez renseigner tout les champs';
             }
 
+            if (!Array.from(inputs).every(input => this.validLetter(input.value))) {
+                throw 'Veuillez n\'utiliser que des lettres';
+            }
+
             inputs.forEach(function(input) {
                 input.classList.add('goodPos');
             });
 
             this.disableLine();
             this.openLine();
+        },
+        validLetter(letter) {
+            let regex = /^[A-Za-z]$/y;
+            return regex.test(letter)
         },
         openLine() {
             let inputs = this.getInputsFromLine(this.essai);
@@ -62,13 +70,13 @@ export default {
 
         getInputsFromLine(line) {
             let tries = document.querySelectorAll('.essai');
-
+            
             return tries[line].querySelectorAll('input');
         }
     },
     template: `
-        <input maxlength="1" 
-               class="sf m-2" 
+        <input maxlength="1"
+               class="sf m-2"
                @keyup=bindValue($event)
                :disabled="essai != 1"></input>
         `
